cfd74c0c5b0c5fb086680c6cc9012e336a38c1b7
Sukanta Bala <sukantabalacste2016@gmail.com>
2020-02-14 22:48:54 +0600

    Sidebar Design updated

--- src/container/includes/AppSidebar.vue
+++ src/container/includes/AppSidebar.vue
@@ -1,15 +1,40 @@
 <template>
   <div class="sidebar">
+
     <ul>
-      <!-- <li class="mt-3"><router-link to="/dashboard"><img src="@/assets/images/kothao-mono.png"></router-link></li> -->
-      <li><i class="fas fa-tachometer-alt"></i> Dashboard</li>
-      <li><i class="fas fa-tachometer-alt"></i> Documentation</li>
-      <li v-b-toggle.accordion-flights><i class="fas fa-tachometer-alt" ></i> Guide</li>
-      <b-collapse class="sidebar-submenu" id="accordion-flights" accordion="my-accordion">
-        <ul class="ml-4">
-          <li>Partners</li>
-          <li>Booking Reports</li>
-          <li>Accounts & Collections <span class="badge badge-danger">fsdf</span></li>
+      <li class="text-center first-list-style">
+        <img src="@/assets/images/kothao-mono.png" width="50px" height="50px">
+      </li>
+      <li><i class="fas fa-tachometer-alt"></i> Dashboard </li>
+      <li><i class="fas fa-book"></i> Documentation</li>
+      <li v-b-toggle.accordion-about-me><i class="fas fa-user-tie"></i> About Me <span class="when-opened"><i class="fas fa-chevron-up"></i></span> <span class="when-closed"><i class="fas fa-chevron-down"></i></span></li>
+      <b-collapse class="sidebar-submenu" id="accordion-about-me" accordion="my-accordion">
+        <ul>
+          <li><i class="fas fa-directions"></i> Manage</li>
+          <li><i class="fas fa-plus"></i> Create</li>
+        </ul>
+      </b-collapse>
+      <li v-b-toggle.accordion-portfolio><i class="fas fa-tasks"></i> Portfolio <span class="when-opened"><i class="fas fa-chevron-up"></i></span> <span class="when-closed"><i class="fas fa-chevron-down"></i></span></li>
+      <b-collapse class="sidebar-submenu" id="accordion-portfolio" accordion="my-accordion">
+        <ul>
+          <li><i class="fas fa-directions"></i> Manage</li>
+          <li><i class="fas fa-plus"></i> Create</li>
+        </ul>
+      </b-collapse>
+
+      <li v-b-toggle.accordion-blog-posts><i class="fas fa-blog"></i> Blog Post <span class="when-opened"><i class="fas fa-chevron-up"></i></span> <span class="when-closed"><i class="fas fa-chevron-down"></i></span></li>
+      <b-collapse class="sidebar-submenu" id="accordion-blog-posts" accordion="my-accordion">
+        <ul>
+          <li><i class="fas fa-directions"></i> Manage</li>
+          <li><i class="fas fa-plus"></i> Create</li>
+        </ul>
+      </b-collapse>
+
+      <li v-b-toggle.accordion-resume-link><i class="far fa-file-pdf"></i> Resume Link <span class="when-opened"><i class="fas fa-chevron-up"></i></span> <span class="when-closed"><i class="fas fa-chevron-down"></i></span></li>
+      <b-collapse class="sidebar-submenu" id="accordion-resume-link" accordion="my-accordion">
+        <ul>
+          <li><i class="fas fa-directions"></i> Manage</li>
+          <li><i class="fas fa-plus"></i> Create</li>
         </ul>
       </b-collapse>
     </ul>
@@ -40,7 +65,7 @@
     position: fixed;
     width: 15%!important;
     height: 100%!important;
-    background: #1B1D29;
+    background: #393836;
     ul {
       list-style-type: none;
       margin: 0px;
@@ -48,12 +73,36 @@
       li {
         word-spacing: 5px;
         font-size: 14px;
-        color: #A8A8A8!important;
+        color: #fff!important;
         text-align: left;
-        padding: 15px;
+        padding: 20px;
+        cursor: pointer;
+        span {
+          float: right;
+        }
+      }
+      .first-list-style {
+        background-color: #292929;
       }
       li:hover {
-        background-color: #ccc;
+        background-color: #111;
+      }
+
+      .sidebar-submenu {
+        background-color: #222;
+        ul {
+          list-style-type: none;
+          margin: 0px;
+          padding: 0px;
+          li {
+            padding-left: 40px;
+            cursor: pointer;
+          }
+          li:hover {
+            background-color: #555;
+          }
+          
+        }
       }
     }
   }
