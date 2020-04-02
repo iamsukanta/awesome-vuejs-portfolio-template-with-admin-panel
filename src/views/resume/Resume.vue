a457c35654f8865595fbb0f6931d3d20fdfcf2ba
SukantaBala28 <sukantabalacste2016@gmail.com>
2020-04-03 00:17:23 +0600

    Resume like created

--- src/views/resume/Resume.vue
+++ src/views/resume/Resume.vue
@@ -2,7 +2,27 @@
   <div class="container-fluid">
     <div class="row">
       <div class="col">
-        <h1>Resume</h1>
+        <h3>Portfolios List</h3>
+        <hr/>
+        <br/>
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">Title</th>
+              <th scope="col">Url</th>
+              <th scope="col">Description</th>
+              <th scope="col">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(portfolio, index) in portfoliosList" :key="index">
+              <th>{{ portfolio.title }}</th>
+              <td>{{ portfolio.url }}</td>
+              <td>{{ portfolio.description }}</td>
+              <td><button class="btn btn-sm btn-warning"><i class="far fa-edit"></i></button> <button class="btn btn-sm btn-danger"><i class="far fa-trash-alt"></i></button> </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   </div>
@@ -10,7 +30,31 @@
 
 <script>
 export default {
-  name: 'Resume'
+  name: 'Portfolios',
+  data() {
+    return {
+      portfoliosList: []
+    }
+  },
+
+  methods: {
+    getAllPortfolios() {
+      this.axios
+      .get(
+        `${process.env.VUE_APP_AWESOME_NODE_API}/resumes/`,
+      )
+      .then(res => {
+        this.portfoliosList = res.data.data;
+        console.log(res.data.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    }
+  },
+  mounted() {
+    this.getAllPortfolios()
+  }
 }
 </script>
 
--- src/views/resume/ResumeCreate.vue
+++ src/views/resume/ResumeCreate.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="container-fluid">
     <div class="row">
-      <div class="col">
-        <h1>Create Resume</h1>
+      <div class="col col-md-6">
+        <h3>Create Resume Link</h3>
+        <hr/>
+        <form @submit.stop.prevent="createResumeLink">
+          <div class="form-group">
+            <label for="web-link">Resume Link</label>
+            <input type="text" class="form-control" id="web-link" v-model="resume.url" placeholder="Enter Resume Link">
+          </div>
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
       </div>
     </div>
   </div>
@@ -10,7 +18,31 @@
 
 <script>
 export default {
-  name: 'CreateResume'
+  name: 'PortfoliosCreate',
+  data() {
+    return {
+      resume: {
+        url: ""
+      }
+    }
+  },
+
+  methods: {
+    createResumeLink() {
+      this.axios
+      .post(
+        `${process.env.VUE_APP_AWESOME_NODE_API}/resumes/create`,
+        this.resume
+      )
+      .then(res => {
+        this.$toastr('success', "Successfully Created.");
+        console.log(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    }
+  }
 }
 </script>
 
